a717e981c5b081a76b874f648e742497b11976bf
Anh Khoi NGUYEN HO <anhkhoi.nguyenho@edu.esiee.fr>
2025-09-26 10:23:59 +0000

    pylint score 8.80, à améliorer : ajout de docstring

--- main.py
+++ main.py
@@ -3,8 +3,47 @@
 
 def ispalindrome(p):
 
-    # votre code ici
-    
+    # Transformer tous les lettres en minuscule
+    p = p.lower()
+
+    # Supprimer les caractères spéciaux
+    # isalmum vérifice is c est alphanumeric
+    p = ''.join(c for c in p if c.isalnum())
+
+    # Supprimer les accents
+    # c
+    p = p.replace('ç','c')
+
+    # o
+    p = p.replace('ô','o')
+
+    # a
+    p = p.replace('à','a')
+    p = p.replace('â','a')
+
+    # i
+    p = p.replace('î','i')
+    p = p.replace('ï','i')
+
+    # u
+    p = p.replace('ù','u')
+    p = p.replace('û','u')
+    p = p.replace('ü','u')
+
+    # e
+    p = p.replace('é','e')
+    p = p.replace('è', 'e')
+    p = p.replace('ê', 'e')
+    p = p.replace('ë', 'e')
+
+
+    # Vérfication de palindrome
+    temp = p[::-1]
+    # print(p)
+    # print(temp)
+    if p == temp:
+        return True
+
     return False
 
 #### Fonction principale
